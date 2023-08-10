30556cc57781db9738c258d36d2daa5b2f7167e3
Виктория Карина <victoria.m.karina@mail.ru>
2023-08-10 18:48:41 +0300

    v15

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -166,7 +166,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = BASE_DIR / "media"
 
 AUTH_USER_MODEL = "users.User"
 
