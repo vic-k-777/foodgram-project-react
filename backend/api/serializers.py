87e49dda41df1f21027999bf3e93b896da877917
Виктория Карина <victoria.m.karina@mail.ru>
2023-08-12 20:37:24 +0300

    m13

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,7 +7,7 @@ from rest_framework.serializers import ModelSerializer
 from django.core.files.base import ContentFile
 from django.db import transaction
 
-from api.views import validate_recipe_name
+from api.validators import validate_recipe_name
 from recipes.models import Ingredient, Recipe, RecipeIngredient, Tag
 from users.models import Subscribe, User
 
